77c8e9a7cc504eee510602a1a1d0b2e20c136601
dykosh <dykosh24@gmail.com>
2022-06-06 02:57:41 +0900

    Applied Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Paper from '@mui/material/Paper';
+
+const root_style = {
+  width: '100%',
+  overflowX: 'auto',
+  marginTop: 'theme.spacing.unit * 3'
+};
+const table_style = {
+  minWidth: '1080'
+};
+
 
 const customers = [
   {
@@ -30,52 +46,25 @@ const customers = [
 ]
 class App extends Component{
   render(){
+    const { classes } = this.props;
     return(
-      <>
-
-        {
-          customers.map(c => {
-            return (
-            <Customer
-              key={c.id}
-              // map함수를 사용할 때에는 key값이 꼭 필요하다.
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />)
-          })
-        }
-
-
-      {/* 하단처럼 일일이 적지 않아도 상단처럼 map함수로 간단히 표기할 수 있다. */}
-      {/* <Customer
-        id={customers[0].id}
-        image={customers[0].image}
-        name={customers[0].name}
-        birthday={customers[0].birthday}
-        gender={customers[0].gender}
-        job={customers[0].job}
-      />
-      <Customer
-        id={customers[1].id}
-        image={customers[1].image}
-        name={customers[1].name}
-        birthday={customers[1].birthday}
-        gender={customers[1].gender}
-        job={customers[1].job}
-      />
-      <Customer
-        id={customers[2].id}
-        image={customers[2].image}
-        name={customers[2].name}
-        birthday={customers[2].birthday}
-        gender={customers[2].gender}
-        job={customers[2].job}
-      /> */}
-      </>
+      <Paper style={{root_style}}>
+        <Table style={{table_style}}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />) })}
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 
