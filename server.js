119b7c26c065ea242b41180dea38b37e6f2fe011
dykosh <dykosh24@gmail.com>
2022-06-06 14:04:26 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,38 +1,35 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+// 서버관련(DB관련) 정보들은 gitignore 처리를 해서 git에 안올라가게 하는 것이 중요!
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database 
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': '홍길동',
-        'birthday': '940724',
-        'gender': '남자',
-        'job': '직장인'
-        },
-        {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': '김도연',
-        'birthday': '940724',
-        'gender': '남자',
-        'job': '프로그래머'
-        },
-        {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': '제일린',
-        'birthday': '950402',
-        'gender': '여자',
-        'job': '주부'
-        }
-      ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
